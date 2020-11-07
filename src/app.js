37c691a8a0135af1fc036db46a121d43f66305ef
Audry-prog <audryavalos@hotmail.com>
2020-11-07 20:21:00 -0300

    arquivo app.js configurado

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
 
-const app = express()
+mongoose.connect("mongodb://localhost:27017/reprograma",  { useNewUrlParser: true, 
+useUnifiedTopology: true 
+});
+
+let db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function () {
+  console.log("Conexão realizada com sucesso.")
+});
 
 //rotas
-const clientes = require("./routes/clientesRoute")
+const clientes = require("./routes/clientesRoute");
 
-//configurar body parser
-app.use(bodyParser.json());
-// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
+
+//app.use(bodyParser.json()); ou
+app.use(express.json()); 
+
+//Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
@@ -17,8 +28,8 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept"
     )
     next()
-  })
+  });
 
 app.use("/clientes", clientes)
 
-module.exports = app
+module.exports = app;
