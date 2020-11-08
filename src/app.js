e22e4284181a8b3f46a4f29b4b60233b4e7a9210
Audry Ávalos <audryavalos@hotmail.com>
2020-11-08 10:34:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/reprograma",  { useNewUrlParser: true, 
+mongoose.connect("mongodb://localhost:27017/clientes",  { useNewUrlParser: true, 
 useUnifiedTopology: true 
 });
 
